01160e183c8c5afb4c0fb101e53ee8d29bcd7a92
Shahid Shaik <galibshahid2008@gmail.com>
2025-07-11 11:11:35 +0530

    Create Alfa_Card_on_Kali_Linux_drivers.sh

---
+++ Alfa_Card_on_Kali_Linux_drivers.sh
@@ -0,0 +1,14 @@
+sudo apt update
+sudo apt upgrade -y
+sudo apt dist-upgrade -y
+sudo apt-get dist-upgrade sudo apt-get install –y linux-headers-$(uname -r)
+sudo reboot now
+sudo apt update
+sudo apt install realtek-rtl88xxau-dkms
+sudo apt install dkms
+git clone https://github.com/aircrack-ng/rtl8812au
+cd rtl8812au/
+make
+sudo make install
+lsusb
+iwconfig
